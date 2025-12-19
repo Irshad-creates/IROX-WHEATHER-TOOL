1d6003be621c0e84f2cd0ced4a1a75f3465aff97
Irshad-creates <ansariirshad78601@gmail.com>
2025-12-19 20:56:49 +0530

    started the wheater app UI with responsiveness

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const API_KEY = "IRSHAD_KEY"
+const API_KEY = "8cf7a694cd9a411f88e104407251912";
 
-async function getweather(city){
-    const  res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${city}`)
-);
-    
-const data = await res.json();
+async function getweather(city) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+  );
 
-// UI update
+  if (!response.ok) {
+    alert("City not found");
+    return;
+  }
+
+  const data = await response.json();
+  console.log(data);
+
+  // current-upper UI
+  document.getElementById("CUL-place").innerHTML =
+    data.location.name;
+
+  // 👇 YAHAN TU KHUD DAY / DATE / MONTH LOGIC DALEGA
+  // document.getElementById("day").innerHTML = ???
+
+  // current-middle UI
   document.getElementById("temp").innerText =
-    Math.round(data.current.temp_c) + "°C";
-                                                                                                                                                                                                                                            d
-  document.getElementById("condition").innerText =
-    data.current.condition.text;
-
-  document.getElementById("weather-icon").src =
-    "https:" + data.cur rent.condition.icon;
-
-
-    document.getElementById("temp").innerText =
     Math.round(data.current.temp_c) + "°C";
 
   document.getElementById("condition").innerText =
@@ -26,3 +30,11 @@ const data = await res.json();
   document.getElementById("weather-icon").src =
     "https:" + data.current.condition.icon;
 }
+
+const input = document.querySelector(".search-box input");
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getweather(input.value.trim());
+  }
+});
